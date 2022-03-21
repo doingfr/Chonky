cffc904f1e8d83868ca9b729fdad5e5ee705d703
Hatem Zehani <hzehani@doing.fr>
2022-03-21 18:33:41 +0100

    dist build

---
+++ packages/chonky-icon-fontawesome/dist/ChonkyIconFA.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2020
+ * @license MIT
+ */
+import { ChonkyIconProps } from 'chonky';
+import React from 'react';
+export declare const ChonkyIconFA: React.FC<ChonkyIconProps>;
